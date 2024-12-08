64d6bbff70ea51035e3ceba10d0961b97eb39a8b
GitHub Action <action@github.com>
2024-12-08 01:54:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/guoyanyong2024/xylose/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/guoyanyong2024/xylose/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.guoyanyong2024.xylose.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/guoyanyong2024/xylose/services/MyProjectService.kt
+++ src/main/kotlin/com/github/guoyanyong2024/xylose/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.guoyanyong2024.xylose.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.guoyanyong2024.xylose.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
